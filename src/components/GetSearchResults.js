98cdeeb58387509a59c309feab207260ab5f6751
Gummy14 <alex.helm@yahoo.com>
2019-04-12 18:15:01 -0400

    CSS Changes

--- src/components/GetSearchResults.js
+++ src/components/GetSearchResults.js
@@ -1,16 +1,19 @@
 let axios = require('axios')
 let BASE_URL = 'https://www.googleapis.com/youtube/v3/search'
 
-module.exports = function (options, callback) {
+export default function (options, callback) {
+  var self = this
   if(!options.apiKey) {
     throw new Error('YouTube API Key Required')
   }  
   axios.get(BASE_URL, { 
     params: {
-      part: 'snippet',
+      part: 'id, snippet',
       key: options.apiKey,
       q: options.searchQuery,
-      type: 'video'
+      type: 'video',
+      videoCategoryId: 10,
+      fields: 'items(id/videoId,snippet(thumbnails/high/url,title))'
     } 
   })
   .then(response => {
@@ -20,5 +23,18 @@ module.exports = function (options, callback) {
   })
   .catch(error => {
     console.log(error)
+    var keyList = self.$store.getters.getApiKeyList
+    var newPosition = self.$store.getters.getApiKeyPosition
+    if (newPosition === 2) {
+      newPosition = 0
+    } else {
+      newPosition++
+    }
+    self.$store.commit('setApiKey', {
+      ApiKey: keyList[newPosition]
+    })
+    self.$store.commit('setApiKeyPosition', {
+      ApiKeyPosition: newPosition
+    })
   })
 }
